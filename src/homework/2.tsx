55a5b22882fdba387c657be234da624449a589f7
PolliBu <8polli.bu@gmail.com>
2024-03-15 19:05:52 +0200

    task 2 requeststep

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,8 +1,10 @@
 import React, { useReducer } from "react";
 
+type RequestStep = "start" | "pending" | "finished" | "idle";
+
 type State = {
   isRequestInProgress: boolean;
-  requestStep: "start" | "pending" | "finished" | "idle";
+  requestStep: RequestStep;
 };
 
 type Action =
